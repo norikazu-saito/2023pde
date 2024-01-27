75808bcd383ed321d5525880b0bf42d65a9feda0
Norikazu Saito <50255317+norikazu-saito@users.noreply.github.com>
2024-01-27 09:52:04 +0900

    revised
    revised

--- Chapter8/femFunc4.m
+++ Chapter8/femFunc4.m
@@ -3,7 +3,7 @@ function z=femFunc4(id, x, y)
     case 0 % right-hand side
     z = 0;
     case 1 % initial data
-    cx=1/4; cy=1/4; p=0.01; M=0.3; r=(x-cx).^2+(y-cy).^2;
+    cx = 1/4; cy = 1/4; p = 0.01; M = 0.3; r = (x-cx).^2+(y-cy).^2;
     z = M*exp(-sqrt(r)/p);
     case 2 % b_1 
     z = sin(pi*x);
