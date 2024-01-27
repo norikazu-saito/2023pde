0d90a422275381f61b272b80816c0122a1107914
Norikazu Saito <50255317+norikazu-saito@users.noreply.github.com>
2024-01-27 09:51:10 +0900

    Update femFunc4.m

--- Chapter8/femFunc4.m
+++ Chapter8/femFunc4.m
@@ -1,16 +1,15 @@
 function z=femFunc4(id, x, y)
-switch id
-    case 0 % right-hand side funtion 
-    z = zeros(size(x));
+  switch id
+    case 0 % right-hand side
+    z = 0;
     case 1 % initial data
-    %z = w;
     cx=1/4; cy=1/4; p=0.01; M=0.3; r=(x-cx).^2+(y-cy).^2;
-    z=M*exp(-sqrt(r)/p);
+    z = M*exp(-sqrt(r)/p);
     case 2 % b_1 
-    z=sin(pi*x);
+    z = sin(pi*x);
     case 3 % b_2 
-    z=sin(pi*y);
+    z = sin(pi*y);
     case 4 % exact solution 
-    z = zeros(size(x)); 
+    z = 0; 
 end    
 end
