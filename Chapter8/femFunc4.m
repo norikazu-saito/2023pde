e184ebe94185549de6c6fcd39b5d036032aa7951
Norikazu Saito <50255317+norikazu-saito@users.noreply.github.com>
2024-01-28 17:05:21 +0900

    revised

--- Chapter8/femFunc4.m
+++ Chapter8/femFunc4.m
@@ -4,7 +4,7 @@ switch id
     z = 0;
     case 1 % initial data
     cx = 1/4; cy = 1/4; p = 0.01; M = 0.3; r = (x-cx).^2+(y-cy).^2;
-    z = M*exp(-sqrt(r)/p);
+    z = M*exp(-r/p);
     case 2 % b_1 
     z = sin(pi*x);
     case 3 % b_2 
